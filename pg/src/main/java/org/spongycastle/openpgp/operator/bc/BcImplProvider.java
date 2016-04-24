0bdb2664aa1dbc341210686931859f77ee463f23
Alexey Guseynov <kibergus@yandex-team.ru>
2016-04-24 18:20:30 +0300

    Disable parts of the library which we don't use

--- pg/src/main/java/org/spongycastle/openpgp/operator/bc/BcImplProvider.java
+++ pg/src/main/java/org/spongycastle/openpgp/operator/bc/BcImplProvider.java
@@ -8,28 +8,28 @@ import org.spongycastle.crypto.BlockCipher;
 import org.spongycastle.crypto.Digest;
 import org.spongycastle.crypto.Signer;
 import org.spongycastle.crypto.Wrapper;
-import org.spongycastle.crypto.digests.MD2Digest;
-import org.spongycastle.crypto.digests.MD5Digest;
+//import org.spongycastle.crypto.digests.MD2Digest;
+//import org.spongycastle.crypto.digests.MD5Digest;
 import org.spongycastle.crypto.digests.RIPEMD160Digest;
 import org.spongycastle.crypto.digests.SHA1Digest;
 import org.spongycastle.crypto.digests.SHA224Digest;
 import org.spongycastle.crypto.digests.SHA256Digest;
 import org.spongycastle.crypto.digests.SHA384Digest;
 import org.spongycastle.crypto.digests.SHA512Digest;
-import org.spongycastle.crypto.digests.TigerDigest;
+//import org.spongycastle.crypto.digests.TigerDigest;
 import org.spongycastle.crypto.encodings.PKCS1Encoding;
 import org.spongycastle.crypto.engines.AESEngine;
 import org.spongycastle.crypto.engines.AESFastEngine;
-import org.spongycastle.crypto.engines.BlowfishEngine;
-import org.spongycastle.crypto.engines.CAST5Engine;
-import org.spongycastle.crypto.engines.CamelliaEngine;
+//import org.spongycastle.crypto.engines.BlowfishEngine;
+//import org.spongycastle.crypto.engines.CAST5Engine;
+//import org.spongycastle.crypto.engines.CamelliaEngine;
 import org.spongycastle.crypto.engines.DESEngine;
 import org.spongycastle.crypto.engines.DESedeEngine;
-import org.spongycastle.crypto.engines.ElGamalEngine;
+//import org.spongycastle.crypto.engines.ElGamalEngine;
 import org.spongycastle.crypto.engines.IDEAEngine;
 import org.spongycastle.crypto.engines.RFC3394WrapEngine;
 import org.spongycastle.crypto.engines.RSABlindedEngine;
-import org.spongycastle.crypto.engines.TwofishEngine;
+//import org.spongycastle.crypto.engines.TwofishEngine;
 import org.spongycastle.crypto.signers.DSADigestSigner;
 import org.spongycastle.crypto.signers.DSASigner;
 import org.spongycastle.crypto.signers.ECDSASigner;
@@ -44,8 +44,8 @@ class BcImplProvider
     {
         switch (algorithm)
         {
-        case HashAlgorithmTags.SHA1:
-            return new SHA1Digest();
+//        case HashAlgorithmTags.SHA1:
+//            return new SHA1Digest();
         case HashAlgorithmTags.SHA224:
             return new SHA224Digest();
         case HashAlgorithmTags.SHA256:
@@ -54,14 +54,14 @@ class BcImplProvider
             return new SHA384Digest();
         case HashAlgorithmTags.SHA512:
             return new SHA512Digest();
-        case HashAlgorithmTags.MD2:
-            return new MD2Digest();
-        case HashAlgorithmTags.MD5:
-            return new MD5Digest();
-        case HashAlgorithmTags.RIPEMD160:
-            return new RIPEMD160Digest();
-        case HashAlgorithmTags.TIGER_192:
-            return new TigerDigest();
+//        case HashAlgorithmTags.MD2:
+//            return new MD2Digest();
+//        case HashAlgorithmTags.MD5:
+//            return new MD5Digest();
+//        case HashAlgorithmTags.RIPEMD160:
+//            return new RIPEMD160Digest();
+//        case HashAlgorithmTags.TIGER_192:
+//            return new TigerDigest();
         default:
             throw new PGPException("cannot recognise digest");
         }
@@ -96,26 +96,26 @@ class BcImplProvider
         case SymmetricKeyAlgorithmTags.AES_256:
             engine = new AESEngine();
             break;
-        case SymmetricKeyAlgorithmTags.CAMELLIA_128:
-        case SymmetricKeyAlgorithmTags.CAMELLIA_192:
-        case SymmetricKeyAlgorithmTags.CAMELLIA_256:
-            engine = new CamelliaEngine();
-            break;
-        case SymmetricKeyAlgorithmTags.BLOWFISH:
-            engine = new BlowfishEngine();
-            break;
-        case SymmetricKeyAlgorithmTags.CAST5:
-            engine = new CAST5Engine();
-            break;
+//        case SymmetricKeyAlgorithmTags.CAMELLIA_128:
+//        case SymmetricKeyAlgorithmTags.CAMELLIA_192:
+//        case SymmetricKeyAlgorithmTags.CAMELLIA_256:
+//            engine = new CamelliaEngine();
+//            break;
+//        case SymmetricKeyAlgorithmTags.BLOWFISH:
+//            engine = new BlowfishEngine();
+//            break;
+//        case SymmetricKeyAlgorithmTags.CAST5:
+//            engine = new CAST5Engine();
+//            break;
         case SymmetricKeyAlgorithmTags.DES:
             engine = new DESEngine();
             break;
         case SymmetricKeyAlgorithmTags.IDEA:
             engine = new IDEAEngine();
             break;
-        case SymmetricKeyAlgorithmTags.TWOFISH:
-            engine = new TwofishEngine();
-            break;
+//        case SymmetricKeyAlgorithmTags.TWOFISH:
+//            engine = new TwofishEngine();
+//            break;
         case SymmetricKeyAlgorithmTags.TRIPLE_DES:
             engine = new DESedeEngine();
             break;
@@ -135,10 +135,10 @@ class BcImplProvider
         case SymmetricKeyAlgorithmTags.AES_192:
         case SymmetricKeyAlgorithmTags.AES_256:
             return new RFC3394WrapEngine(new AESFastEngine());
-        case SymmetricKeyAlgorithmTags.CAMELLIA_128:
-        case SymmetricKeyAlgorithmTags.CAMELLIA_192:
-        case SymmetricKeyAlgorithmTags.CAMELLIA_256:
-            return new RFC3394WrapEngine(new CamelliaEngine());
+//        case SymmetricKeyAlgorithmTags.CAMELLIA_128:
+//        case SymmetricKeyAlgorithmTags.CAMELLIA_192:
+//        case SymmetricKeyAlgorithmTags.CAMELLIA_256:
+//            return new RFC3394WrapEngine(new CamelliaEngine());
         default:
             throw new PGPException("unknown wrap algorithm: " + encAlgorithm);
         }
@@ -155,10 +155,10 @@ class BcImplProvider
         case PGPPublicKey.RSA_GENERAL:
             c = new PKCS1Encoding(new RSABlindedEngine());
             break;
-        case PGPPublicKey.ELGAMAL_ENCRYPT:
-        case PGPPublicKey.ELGAMAL_GENERAL:
-            c = new PKCS1Encoding(new ElGamalEngine());
-            break;
+//        case PGPPublicKey.ELGAMAL_ENCRYPT:
+//        case PGPPublicKey.ELGAMAL_GENERAL:
+//            c = new PKCS1Encoding(new ElGamalEngine());
+//            break;
         case PGPPublicKey.DSA:
             throw new PGPException("Can't use DSA for encryption.");
         case PGPPublicKey.ECDSA:
